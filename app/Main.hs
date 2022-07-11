d00d99a60a45380773eccbd583a102495b60ad4c
nikonru <makaroff.nickita2013@yandex.ru>
2022-07-12 00:11:09 +0300

    Added base form for programm

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,13 @@
 module Main where
 
+import qualified Codec.Picture as CP
+import qualified System.IO as SIO
+import Data.Either
+import qualified Data.Vector.Storable as V
+
+imagePath :: SIO.FilePath
+imagePath = "./Sakuya[WhoAmI].jpg"
+
 type Harmonic = (Float, Float) 
 
 countSampleNumber :: Int -> Int -> Int
@@ -51,4 +59,47 @@ getCleanAudio scale sampleRate pic = map (\x->x/norm) audio
 toHarmonic :: Float -> Float -> Harmonic
 toHarmonic amp freq = (amp, freq)
 
-main = print (show (getCleanAudio 20 44100 [[1,1,1],[2,3,4]]))
+getDynSize :: CP.DynamicImage -> (Int, Int)
+getDynSize img = (dynWidth img, dynHeight img)
+
+dynWidth :: CP.DynamicImage -> Int
+dynWidth img = CP.dynamicMap CP.imageWidth img
+
+dynHeight :: CP.DynamicImage -> Int
+dynHeight img = CP.dynamicMap CP.imageHeight img
+
+extractPixels :: CP.Image CP.PixelRGB16 ->  [[Float]]
+extractPixels img = extractPixels' img width height [] 
+                    where width = CP.imageWidth img
+                          height = CP.imageHeight img
+
+extractPixels' :: CP.Image CP.PixelRGB16 -> Int -> Int ->  [[Float]] -> [[Float]]
+extractPixels' img x y list = if y>0
+                                then extractPixels' img x (y-1) (value:list)
+                                else list
+                              where value = extractPixelsLine img x y []
+
+extractPixelsLine :: CP.Image CP.PixelRGB16 -> Int -> Int ->  [Float] -> [Float]
+extractPixelsLine img x y list = if x>0
+                                   then extractPixelsLine img (x-1) y (value:list)
+                                   else list
+                                  where value = getPixel img x y
+
+getPixel img x y = convertPixelToGrayscale (CP.pixelAt img x y)
+{-TODO overload-}
+convertPixelToGrayscale :: CP.PixelRGB16 -> Float
+convertPixelToGrayscale (CP.PixelRGB16 r g b) = 0.299*red + 0.587*green + 0.114*blue
+                                               where red = fromIntegral r
+                                                     green = fromIntegral g
+                                                     blue = fromIntegral b
+
+doJob :: Int -> Int -> CP.Image CP.PixelRGB16 -> [Float]
+doJob scale sampleRate img = getCleanAudio scale sampleRate pic
+                             where pic = extractPixels img
+
+main = do 
+        image <- CP.readImage imagePath
+        case image of
+            Left err -> putStrLn ("Could not read image: " ++ err)
+            Right img -> putStrLn ("Success: " ++ (show (doJob 20 44100 (CP.convertRGB16 img))))
+        
