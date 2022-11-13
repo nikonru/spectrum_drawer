a227e34562a71fcc6f01ddf21262afb2b0d517d0
nikonru <makaroff.nickita2013@yandex.ru>
2022-11-13 21:52:28 +0300

    Added TODO

--- Main.hs
+++ Main.hs
@@ -2,15 +2,15 @@ module Main where
 
 import System.Environment
 
-
 import ImageReader (convertImageToWave)
 
 path = "tree.png"
 
 parsePath args = head args
-                   
+--TODO add command-line interface
+--TODO investigate memory overusage for 143x80 png file with dt = 1 sec and df = 240 Hz                   
 main :: IO ()
 main = do
          args <- getArgs
          --let path = parsePath args
-         convertImageToWave path "test.wav" 0.01 240
+         convertImageToWave path "test.wav" 0.1 240
